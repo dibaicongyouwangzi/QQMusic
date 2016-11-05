fb3b7800bf8bd0a28cb66d57f8f2cca62562668e
KB <meipaigu@163.com>
2016-11-05 18:15:48 +0800

    界面赋值方案分析

--- QQMusic/QQMusic/Classes/QQDetail/Controller/QQDetailVC.swift
+++ QQMusic/QQMusic/Classes/QQDetail/Controller/QQDetailVC.swift
@@ -16,14 +16,57 @@ class QQDetailVC: UIViewController {
     // 歌词的视图
     var lrcView: UIView?
     
-    @IBOutlet weak var foreImageView: UIImageView!
     
+    // 分析界面，根据不同的更新频率，采用不同的方案赋值
+    /** 歌词动画背景 1 */
+    @IBOutlet weak var foreImageView: UIImageView!
+    /** 背景图片 1 */
+    @IBOutlet weak var backImageView: UIImageView!
+    /** 歌曲名称 1 */
+    @IBOutlet weak var songNameLabel: UILabel!
+    /** 歌手名称 1 */
+    @IBOutlet weak var singNameLabel: UILabel!
+    /** 总时长 1 */
+    @IBOutlet weak var totalTimeLabel: UILabel!
+    
+    /** 歌词label n */
     @IBOutlet weak var lrclabel: UILabel!
+    /** 已经播放时长 n */
+    @IBOutlet weak var costTimeLabel: UILabel!
+    /** 进度条 n */
     @IBOutlet weak var progressSlider: UISlider!
+    
+   
+    @IBAction func close() {
+    }
+    @IBAction func playOrPause(_ sender: UIButton) {
+    }
+    @IBAction func preMusic() {
+        setupOnce()
+    }
+    @IBAction func nextMusic() {
+        setupOnce()
+    }
+    
+    // 负责更新很多次的timer
+    var timer: Timer?
 }
 
 // MARK:- 业务逻辑
 extension QQDetailVC {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupOnce()
+        
+        addTimer()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        removerTimer()
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,6 +75,40 @@ extension QQDetailVC {
         setupLrcScrollView()
         setSlider()
     }
+    
+    
+    func setupOnce() -> (){
+        foreImageView.image = nil
+        /** 背景图片 1 */
+        backImageView = nil
+        /** 歌曲名称 1 */
+        songNameLabel.text = ""
+        /** 歌手名称 1 */
+        singNameLabel.text = ""
+        /** 总时长 1 */
+        totalTimeLabel.text = ""
+    }
+    
+    func setupTimes() -> (){
+        /** 歌词label n */
+        lrclabel.text = ""
+        /** 已经播放时长 n */
+        costTimeLabel.text = ""
+        /** 进度条 n */
+        progressSlider.value = 0
+    }
+    
+    
+    func addTimer() -> (){
+        timer = Timer(timeInterval: 1.0, target: self, selector: #selector(QQDetailVC.setupTimes), userInfo: nil, repeats: true)
+        RunLoop.current.add(timer!, forMode: .commonModes)
+    }
+    
+    func removerTimer() -> (){
+        timer?.invalidate()
+        timer = nil
+    }
+    
 }
 
 // MARK:- 界面操作
